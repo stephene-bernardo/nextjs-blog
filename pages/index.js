fdbe0eedd41633353c53e1652823018a58793e15
Karl Bernardo <stephene.bernardo@gmail.com>
2020-11-23 01:32:34 +0800

    dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import Layout, { siteTitle } from '../components/Layout'
 import utilStyles from '../styles/utils.module.css'
 import {getSortedPostsData} from '../lib/posts'
+import Date from '../components/date'
 
 export default function Home({allPostsData}) {
   return (
@@ -22,11 +23,16 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({id, date, title}) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
+
+              <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br/>
+              <small className={utilStyles.lightText}>
+                <Date dateString={date}/>
+              </small>
+            </li>
             </li>
           ))}
         </ul>
